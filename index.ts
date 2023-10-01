69232c69e553e38e1d2d7b2f0ee2808afaa62262
Germano Lira <dolan.3@hotmail.com>
2023-09-30 23:38:33 -0300

    Atualizar o index.ts

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
-const language: string = "Typescript
-console.log(language)
+const language: string = "Typescript";
+console.log(language);
+
+// using function
+
+interface Props {
+  language: string;
+}
+
+function SelectLanguage({ language }: Props) {
+  console.log(language);
+}
+
+SelectLanguage({ language });
